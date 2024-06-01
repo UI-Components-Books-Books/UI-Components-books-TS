63014efa8327964bb551944a0553899514ec8617
Frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2024-05-31 22:31:11 -0500

    fix(row): fix row component functonality

--- src/components/Row/src/row.tsx
+++ src/components/Row/src/row.tsx
@@ -1,18 +1,35 @@
-import { forwardRef } from 'react'
+import { forwardRef } from "react";
 
-import './row.css'
-import classnames from 'classnames';
+import "./row.css";
+import classnames from "classnames";
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
   addClass?: string;
-  justifyContent?: string;
-  alignItems?: string;
+  justifyContent?:
+    | "center"
+    | "flex-start"
+    | "flex-end"
+    | "space-between"
+    | "space-around"
+    | "space-evenly";
+  alignItems?: "center" | "flex-start" | "flex-end" | "baseline" | "stretch";
+  flexDirection?: "row" | "row-reverse" | "column" | "column-reverse";
 }
 
-export const Row = forwardRef<HTMLDivElement, Props>(
-  function Row({ addClass, ...props }, ref: React.Ref<HTMLDivElement>) {
-    return (
-      <div ref={ref} className={classnames('c-row', { [addClass ?? ""]: addClass })} {...props} />
-    );
-  }
-);
+export const Row = forwardRef<HTMLDivElement, Props>(function Row(
+  { addClass, justifyContent, alignItems, flexDirection, ...props },
+  ref: React.Ref<HTMLDivElement>
+) {
+  return (
+    <div
+      ref={ref}
+      className={classnames("c-row", {
+        [addClass ?? ""]: addClass,
+        [`c-row--align-items-${alignItems}`]: alignItems,
+        [`c-row--justify-content-${justifyContent}`]: justifyContent,
+        [`c-row--flex-direction-${flexDirection}`]: flexDirection,
+      })}
+      {...props}
+    />
+  );
+});
