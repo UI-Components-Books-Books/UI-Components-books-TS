c1e2a8869f5c042cb6bbc3446919406c134f2095
Frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2024-06-04 12:39:52 -0500

    refactor(image): refactor Image component structure

--- src/components/Image/src/image.tsx
+++ src/components/Image/src/image.tsx
@@ -1,73 +1,67 @@
-import { useState, forwardRef } from 'react'
+import { useState, forwardRef } from "react";
 
-import classnames from 'classnames'
+import classnames from "classnames";
 
-import { imageBase } from './image-base'
-import './image.css'
+import { imageBase } from "./image-base";
+import "./image.css";
 
 interface Props {
-    src?: string,
-    alt?: string,
-    title?: string,
-    size?: string,
-    addClass?: string,
-    noCaption?: boolean,
+  src?: string;
+  alt?: string;
+  title?: string;
+  size?: string;
+  addClass?: string;
+  noCaption?: boolean;
 }
 
-export const Image: React.FC<Props> = forwardRef<HTMLImageElement, Props>(function Image({
-    src = 'none',
-    alt = '',
-    title = 'Image 1.',
-    size,
-    addClass,
-    noCaption = false,
-    ...props
-}, ref) {
-
+export const Image: React.FC<Props> = forwardRef<HTMLImageElement, Props>(
+  function Image(
+    {
+      src = "none",
+      alt = "",
+      title = "Image 1.",
+      size,
+      addClass,
+      noCaption = false,
+      ...props
+    },
+    ref
+  ) {
     // Estado para determinar si se muestra la imagen por defecto.
-    const [error, setError] = useState<boolean>(false)
+    const [error, setError] = useState<boolean>(false);
 
     /**
-     * Función de error, al no conseguir la imagen cambia el estado setError
-     * y muestra la imagen por defecto.
-     *
+     * Manejador de error para la imagen.
+     * Si la imagen no se carga correctamente, cambia el estado `error`
+     * para mostrar la imagen por defecto.
      */
     const onError = (): void => {
-        setError(!error)
-    }
+      setError(true);
+    };
 
-    /**
-     * Condicionamos que imagen se va a mostrar
-     * dependiendo del valor del estado error.
-     */
-    const imgToSee = !error ? src : imageBase
+    // Determina la fuente de la imagen a mostrar dependiendo del estado `error`.
+    const imgToSee = !error ? src : imageBase;
 
-
+    // Elemento HTML a renderizar, puede ser `div` o `figure` basado en `noCaption`.
+    const Element = noCaption ? "div" : "figure";
 
     return (
-        <figure
-            className={classnames('c-image', { [addClass ?? ""]: addClass })}
-            {...(size && { style: { '--image-max-width': size } as React.CSSProperties })}
-        >
-            <img
-                ref={ref}
-                src={imgToSee}
-                alt={alt}
-                onError={onError}
-                {...props}
-            />
-
-            {!noCaption && (
-                <figcaption className='c-image__figcaption'>
-                    <p>
-                        { title ? <strong>{title}&nbsp;</strong> : null}
-                        {alt}
-                    </p>
-                </figcaption>
-            )}
-        </figure>
-    )
-})
-
-
-
+      <Element
+        className={classnames("c-image", { [addClass ?? ""]: addClass })}
+        {...(size && {
+          style: { "--image-max-width": size } as React.CSSProperties,
+        })}
+      >
+        <img ref={ref} src={imgToSee} alt={alt} onError={onError} {...props} />
+        {!noCaption && (
+          <figcaption className="c-image__figcaption">
+            <p>
+              {title ? <strong>{title}&nbsp;</strong> : null}
+              {alt}
+            </p>
+          </figcaption>
+        )}
+      </Element>
+    );
+  }
+);
